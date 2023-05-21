edbe654eca9d0a886efd9bf8cc943958ae264998
Oleksii Khrushch <oleksiikhr@outlook.com>
2023-05-21 13:50:19 +0300

    fix docker hmr

--- Makefile
+++ Makefile
@@ -7,5 +7,8 @@ up:
 down:
 	docker-compose down
 
+restart:
+	docker-compose restart
+
 logs:
 	docker-compose logs -f
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,3 +1,27 @@
+import {HmrOptions} from "vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-})
+  vite: {
+    server: {
+      hmr: Object.fromEntries(Object.entries({
+        host: process.env.HMR_HOST,
+        protocol: process.env.HMR_PROTOCOL,
+        path: process.env.HMR_PATH,
+        port: undefined === process.env.HMR_PORT ? undefined : +process.env.HMR_PORT,
+        clientPort: undefined === process.env.HMR_CLIENT_PORT ? undefined : +process.env.HMR_CLIENT_PORT,
+      } as HmrOptions).filter(([_, value]) => value !== undefined))
+    },
+    plugins: [
+      {
+        name: 'clientHost',
+        transform(code, id) {
+          // https://github.com/vitejs/vite/issues/8666
+          if (process.env.HMR_CLIENT_HOST && id.endsWith('dist/client/client.mjs')) {
+            return code.replace('__HMR_HOSTNAME__', JSON.stringify(process.env.HMR_CLIENT_HOST))
+          }
+        }
+      }
+    ],
+  },
+});
